bcd8ca2e03d9fc580e7182c23a3d13ee3bc5c0ad
Henrik Joreteg <hjoreteg@gmail.com>
2015-11-10 14:52:45 -0500

    Tue Nov 10 14:52:45 EST 2015

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-document.write('Hello edUiconf!')
+var React = require('react')
+var ReactDom = require('react-dom')
+
+
+var Hello = React.createClass({
+  render: function () {
+    return <div>hello, {this.props.name}</div>
+  }
+})
+
+ReactDom.render(<Hello name='edUi'/>, document.body)
